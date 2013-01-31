39c1517516508892607e7bed230d12b44738bdb3
Antho <anthony.vernagallo@gmail.com>
2013-01-31 16:09:42 +0100

    test_mem1 : meilleur affichage

--- code/test/test_mem1.c
+++ code/test/test_mem1.c
@@ -11,6 +11,7 @@ int main() {
 	char *tab2;
 
 	// --------- initialisation -----------
+	printf("Initialisation...\n");
 	if (mem_init(NBINIT) == NULL) {
 		printf("Erreur : tas plein OU plus de frame libres\n");
 		return -1;
@@ -19,45 +20,50 @@ int main() {
 
 	// ----------- tab 1 ------------------
 	// allocation de NB1 entiers
+	i = NB1;
+	printf("Allocation de tab1 : %d entiers\n", &i);
 	tab1 = (int*) mem_alloc(sizeof (int) * NB1);
 	if (tab1 == NULL) {
-		printf("Erreur : mémoire allouée pleine\n");
+		printf("Erreur : mémoire allouée pleine !\n");
 		return 1;
 	}
 
-	printf("ECRITURE TAB1\n");
+	printf("Écriture dans tab1...\n");
 	for (i = 0; i < NB1; i++)
 		tab1[i] = i;
 
-	printf("LECTURE TAB1\n");
+	printf("Lecture de tab1 :\n");
 	for (i = 0; i < NB1; i++)
-		printf("tab1[%d] = %d\n", &i, &tab1[i]);
+		printf("tab1[%d] = %d [%s]\n", &i, &tab1[i], tab1[i]==i ? "OK" : "ERREUR");
 	// ------------------------------------
 
 	// ----------- tab 2 ------------------
 	// allocation de NB2 caractères
+	i = NB2;
+	printf("Allocation de tab2 : %d caractères\n", &i);
 	tab2 = (char*) mem_alloc(sizeof (char) * NB2);
 	if (tab2 == NULL) {
-		printf("Erreur : mémoire allouée pleine\n");
-		printf("Libération de tab1\n");
+		printf("Erreur : mémoire allouée pleine !\n");
+		printf("Libération de tab1...\n");
 		mem_free(tab1);
 	}
+	printf("Allocation de tab2 : %d caractères\n", &i);
 	tab2 = (char*) mem_alloc(sizeof (char) * NB2);
 	if (tab2 == NULL) {
 		printf("Erreur : mémoire allouée pleine\n");
 		return 2;
 	}
 
-	printf("ECRITURE TAB2\n");
+	printf("Écriture dans tab2...\n");
 	for (i = 0; i < NB2; i++)
 		tab2[i] = 'A' + i;
 
-	printf("LECTURE TAB2\n");
+	printf("Lecture de tab2 :\n");
 	for (i = 0; i < NB2; i++)
-		printf("tab2[%d] = %c\n", &i, &tab2[i]);
+		printf("tab2[%d] = %c [%s]\n", &i, &tab2[i], tab2[i]=='A'+i ? "OK" : "ERREUR");
 	// ------------------------------------
 
 
-	printf("FIN\n");
+	printf("FIN DU TEST\n");
 	return 0;
 }
